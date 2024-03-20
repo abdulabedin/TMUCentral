6b98d48f6dfd17862001386ea3f8a0d639d538c3
Peter-012 <151083213+Peter-012@users.noreply.github.com>
2024-03-19 21:52:09 -0400

    Made the code clean

--- tmucentral/Backend/Database/model.js
+++ tmucentral/Backend/Database/model.js
@@ -2,15 +2,15 @@ const {Schema, model} = require('mongoose');
 
 const ObjectId = Schema.Types.ObjectId;
 
-const userSchema = new Schema({
+const User = model('User', new Schema({
     name: String,
     studentID: Number,
     username: String,
     password: String,
     email: String
-});
+}));
 
-const productSchema = new Schema({
+const Product = model('Product', new Schema({
     sold: Boolean,
     datePosted: Date,
     title: String,
@@ -19,29 +19,23 @@ const productSchema = new Schema({
     category: String,
     description: String,
     image: String
-});
+}));
 
-const sellerSchema = new Schema({
+const Seller = model('Seller', new Schema({
     userID: ObjectId,
     productID: ObjectId
-});
+}));
 
-const buyerSchema = new Schema({
+const Buyer = model('Buyer', new Schema({
     userID: ObjectId,
     productID: ObjectId
-});
+}));
 
-const transactionSchema = new Schema({
+const Transaction = model('Transaction', new Schema({
     productID: ObjectId,
     sellerID: ObjectId,
     buyerID: ObjectId,
     datePurchased: Date
-});
-
-const User = model('User', userSchema);
-const Product = model('Product', productSchema);
-const Seller = model('Seller', sellerSchema);
-const Buyer = model('Buyer', buyerSchema);
-const Transaction = model('Transaction', transactionSchema);
+}));
 
 module.exports = {User, Product, Seller, Buyer, Transaction}
